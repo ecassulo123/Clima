1712b10b4b87c040e74b43e09219ade8afbbe28b
Ethan Cassulo <ethancassulo@ethans-macbook-air-2.local>
2019-02-06 13:35:47 -0800

    Initial Commit

---
+++ Clima/ChangeCityViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ChangeCityViewController.swift
+//  WeatherApp
+//
+//  Created by Angela Yu on 23/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+
+
+protocol ChangeCityDelegate {
+    func userEnteredANewCityName(city: String)
+}
+
+
+class ChangeCityViewController: UIViewController {
+    
+    var delegate : ChangeCityDelegate?
+    
+    //linked IBOutlet to text field
+    @IBOutlet weak var changeCityTextField: UITextField!
+
+    
+    //IBAction for "get weather" button
+    
+    
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+
+        let cityName = changeCityTextField.text!
+        
+        delegate?.userEnteredANewCityName(city: cityName)
+
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+
+    //IBAction for "back" button
+    ChangeCityViewController.
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
